9adc9226bd7f4a95ef624b07f562032ff2b9dfb2
Kyle BAMPING <kbamping@e5r10p8.wethinkcode.co.za>
2017-08-28 10:28:21 +0200

    Removed ZombieGame

--- ZombieGame/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "MainGame.h"
-
-int main(void) {
-    MainGame mainGame;
-    mainGame.run();
-
-    return 0;
-}
--- ZombieGame/uninstall_dependencies.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-brew uninstall SDL2_ttf
-brew uninstall SDL2_image
-brew uninstall SDL2_mixer
-brew uninstall SDL2
-brew uninstall GLEW
-#brew uninstall pkg-config
